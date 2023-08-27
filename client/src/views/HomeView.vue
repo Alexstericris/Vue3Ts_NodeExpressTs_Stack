311ed6f32c0376e89071b6984c0af04ea714ea19
Alex <scristea@students.uni-mainz.de>
2023-08-28 00:30:31 +0200

    change to composition api

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -20,13 +20,6 @@ function mytest() {
 </script>
 <template>
     <main>
-      <div :key="index" v-for="(test,index) in tests" class="mt-4 col-lg-3">
-        <div class="h-100 p-5 bg-light border rounded-3 text-center">
-          <h2>{{ test.name }}</h2>
-        </div>
-      </div>
-      <button @click="add()">t1</button>
-      <button @click="mytest()">t2</button>
         <div class="container mt-5 py-4 text-center">
             <div class="flex flex-wrap justify-content-center">
                     <a class="btn text-nowrap mt-3 px-10 mx-2 py-5 bg-light rounded text-decoration-none" href="https://www.numeriqberry.com">
