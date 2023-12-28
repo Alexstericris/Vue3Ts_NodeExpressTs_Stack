f2902f8c753c2ed75f57f728e3d8d1845a40df70
Alex <scristea@students.uni-mainz.de>
2023-12-28 19:36:48 +0100

    debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,24 @@
 import errorHandler from "errorhandler";
 import app from "./app";
 import {registerSocketIo} from "./ioserver";
+import logger from "./util/logger";
 
 
 /**
  * Error Handler. Provides full stack
  */
-if (process.env.NODE_ENV === "development") {
-    app.use(errorHandler());
-}
+// if (process.env.NODE_ENV === "development") {
+app.use(errorHandler());
+// }
 
 
 /**
  * Start Express server.
  */
 const server = app.listen(app.get("port"), () => {
+    logger.debug(`App is running at http://localhost:${app.get("port")} in ${app.get("env")} mode`)
     console.log(
-        "  App is running at http://localhost:%d in %s mode",
+        "App is running at http://localhost:%d in %s mode",
         app.get("port"),
         app.get("env")
     );
